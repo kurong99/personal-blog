f11c2b7d346ccf542b812064daab3128c4cdf26d
tengrui <1470909186@qq.com>
2024-07-31 00:27:54 +0800

    <add> Install plugins

--- personal-blog/src/main.js
+++ personal-blog/src/main.js
@@ -5,15 +5,17 @@ import store from './store'
 // 引入VueRouter
 import VueRouter from 'vue-router'
 import router from '../src/router'
+// 引入转码插件
+import { Base64 } from 'js-base64';
 // 引入全局样式
 import '@/assets/css/theme.css'
 
-Vue.config.productionTip = false
-
 Vue.use(VueRouter);
+Vue.use(Base64);
 
 new Vue({
   render: h => h(App),
   store,
-  router
+  router,
+  Base64
 }).$mount('#app')
--- personal-blog/vue.config.js
+++ personal-blog/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://api.github.com/repos/kurong99/kurong99.github.io/contents/pic/03d050acd661fc087249e7671b9ad528.png',
+  //       pathRewrite: {'^/api': ''},
+  //       ws: true,
+  //       changeOrigin: true
+  //     },
+  //   }
+  // }
 })
