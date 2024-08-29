1060b18072b32af33a8ef016826e962d1b9eafa3
tengrui <1470909186@qq.com>
2024-08-29 23:25:46 +0800

    <update> hidden secret

--- src/utills/common.js
+++ src/utills/common.js
@@ -1,35 +1,28 @@
 import axios from "axios"
 import { nanoid } from "nanoid";
 import { marked } from "marked";
-import store from "@/store";
+// import store from "@/store";
 import Utills from '../utills/directory'
 
 const data = [];
 const { getDirectory, getHeadings} = Utills;
-const TOKEN  = store.state.GITHUB_TOKEN;
+// const TOKEN  = store.state.GITHUB_TOKEN;
 const getCommon = async () => {
 
     try {
-        const date = await axios('https://api.github.com/repos/kurong99/plog',{
-            headers:{
-                'Authorization': `token ${TOKEN}`
-            }
-        });
+        // const date = await axios('https://api.github.com/repos/kurong99/plog',{
+        //     headers:{
+        //         'Authorization': `token ${TOKEN}`
+        //     }
+        // });
+        const date = await axios('https://api.github.com/repos/kurong99/plog');
         console.log(date);
         
-        const response = await axios.get('https://api.github.com/repos/kurong99/plog/contents/2024/common',{
-            headers:{
-                'Authorization': `token ${TOKEN}`
-            }
-        });
+        const response = await axios.get('https://api.github.com/repos/kurong99/plog/contents/2024/common');
         if (response.status == 200) {
             const list = response.data;
             for (const item of list) {
-                const response = await axios.get(`https://api.github.com/repos/kurong99/plog/contents/2024/common/${item.name}`,{
-                    headers:{
-                        'Authorization': `token ${TOKEN}`
-                    }
-                });
+                const response = await axios.get(`https://api.github.com/repos/kurong99/plog/contents/2024/common/${item.name}`);
                 if (response) {
                     const base64Content = response.data.content;
                     const Base64 = require('js-base64').Base64;
