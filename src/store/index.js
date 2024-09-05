2d0bf3991aa309d3b85fab687b627a121fb4a254
tengrui <1470909186@qq.com>
2024-09-05 22:35:37 +0800

    <update> renew test token

--- src/store/index.js
+++ src/store/index.js
@@ -27,7 +27,7 @@ const store = {
       },
       state: {
         theme: theme,
-        GITHUB_TOKEN: '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'
+        GITHUB_TOKEN: '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'
       },
 }
 
